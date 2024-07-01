3192e314216b1849f8eeac2628aa8f637bb3ee11
RBoorsma <93580036+RBoorsma@users.noreply.github.com>
2024-07-01 22:41:55 +0200

    update

--- Messaging/DataTypes/RabbitMQData.cs
+++ Messaging/DataTypes/RabbitMQData.cs
@@ -2,7 +2,7 @@
 
 internal static class RabbitMQData
 {
-    internal static Uri Uri = new Uri("amqps://bwyyntlu:shit-MNW0k86-nzp2QX4YAvM03YpVLmF@sparrow.rmq.cloudamqp.com/bwyyntlu");
+    internal static Uri Uri = new Uri("");
     internal static string hostname = "localhost";
     internal static string user = "user";
     internal static string password = "password";
